47cc7a624623668cded93a99076dfc85f87b69b2
MaximNikiforov <modolock@gmail.com>
2022-05-19 02:10:41 +0300

    Update hw-4.2.py

--- hw-4.2.py
+++ hw-4.2.py
@@ -2,6 +2,6 @@
 # значения которых больше предыдущего элемента.
 
 my_list = list(map(int, input('Введите список чисел через пробел: ').split()))
-new_list = [el for (index, el) in enumerate(my_list) if my_list.index(el) > 0 and el > my_list[index-1]]
+new_list = [el for (index, el) in enumerate(my_list) if index > 0 and el > my_list[index-1]]
 print(my_list)
 print(new_list)
