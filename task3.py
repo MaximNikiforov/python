78c9422b91ced1e6503211a5e7e863b03a34c459
MaximNikiforov <104123653+MaximNikiforov@users.noreply.github.com>
2022-11-29 22:37:03 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,3 +1,9 @@
-#
+# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
 
+from random import randint
+
+test_list = [randint(0, 10) for _ in range(20)]
+print(f'Дано: \n{test_list}')
+
+print((lambda x: list(set(x)))(test_list))
